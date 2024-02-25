728e80edba857267813283de9ae3248471b29277
Inupa6677 <inupa001@gmail.com>
2024-02-25 23:04:46 +0530

    added the clusterd map

--- connecting model/static/jsFiles/aaa.js
+++ connecting model/static/jsFiles/aaa.js
@@ -4,6 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
 function focusMapOnDistrict(district) {
     // dictionary to map district with coordinates
     var districtCoordinates = {
